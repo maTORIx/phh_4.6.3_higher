7d112ecf685bbee8fd04eebcb24b408a6c0aa4a9
maTORIx <matorixmatrix@gmail.com>
2017-07-10 16:00:48 +0900

    高階関数の活用

--- 4_45.js
+++ 4_45.js
@@ -8,6 +8,10 @@ function showElement(value, key) {
     console.log(key + " : " + value)
 }
 
+function showDecolatedElement(value, key) {
+    console.log(`キー(${key}) : 値(${value})`)
+}
+
 var ary = [1,2,3,4,5];
-arrayWalk(ary,showElement);
+arrayWalk(ary,showDecolatedElement);
 // ary.forEach(showElement);
